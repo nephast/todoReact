2d7f64eea3e69eb547f8b97f52f23fe818def0d4
Stéphan Gaud <stephgoeseast@gmail.com>
2018-01-27 19:11:18 +0000

    first commit - Includes react server + API (with vanilla JS front end)

---
+++ todoreact/src/todoList.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import TodoItem from './todoItem';
+const APIURL = '/api/todos';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        }
+    }
+
+    componentWillMount() {
+       this.loadTodos();
+    }
+
+    loadTodos() {
+        fetch(APIURL)
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >=400 && resp.status < 500) {
+                    return resp.json()
+                    .then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    })
+                } else {
+                    let err = {errorMessage: 'Server is down, pls try again later'};
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(todos => this.setState({todos}));   
+    }
+
+    render() {
+        const todos = this.state.todos.map((t) => (
+            <TodoItem
+            key={t._id}
+            {...t}
+            />
+        ));
+        return (
+            <div>
+              <h1> Todo List! </h1>
+              <ul>
+               {todos}
+              </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
